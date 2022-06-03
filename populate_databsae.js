e85826d49cb7fa2024967321a59e078b83bef4ba
Litoninot <litoninot12345@gmail.com>
2022-06-03 10:17:24 +0700

    add transaction

--- populate_databsae.js
+++ populate_databsae.js
@@ -5,30 +5,127 @@ try{
       {    
           "table" : "users",
           "fields" : ["name","email","code"],
-          "datas" : [
-            ["Admin","admin@gmail.com",1],           
-            ["User","user@gmail.com",2]
-          ],
+          "range" : {
+            "value" : 10,
+            "values" : ["User-","user@gmail.com-","Code-"]
+          },                 
           "sqlTable" : `CREATE TABLE users (
               id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
               code varchar(50) NOT NULL UNIQUE,
               name varchar(50) NOT NULL,
-              email varchar(50) NOT NULL UNIQUE
+              email varchar(50) NOT NULL UNIQUE,
+              created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+              updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+              deleted_at TIMESTAMP NULL
           );`
       },
+
       {    
         "table" : "suppliers",
         "fields" : ["name","code"],
-        "datas" : [
-          ["Sup1",1],           
-          ["Sup2",2]
-        ],
+        "range" : {
+          "value" : 10,
+          "values" : ["Sup-","Code-"]
+        },                
         "sqlTable" : `CREATE TABLE suppliers (
             id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
             code varchar(50) NOT NULL UNIQUE,
-            name varchar(50) NOT NULL
+            name varchar(50) NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+            deleted_at TIMESTAMP NULL
         );`
-    },
+      },
+
+      {    
+        "table" : "warehouses",
+        "fields" : ["name","code"],
+        "range" : {
+          "value" : 10,
+          "values" : ["Wr-","Code-"]
+        },                
+        "sqlTable" : `CREATE TABLE warehouses (
+            id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+            code varchar(50) NOT NULL UNIQUE,
+            name varchar(50) NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+            deleted_at TIMESTAMP NULL
+        );`
+      },
+
+      {    
+        "table" : "customers",
+        "fields" : ["name","code","type"],
+        "range" : {
+          "value" : 10,
+          "values" : ["Cr-","Code-","Type-"]
+        },          
+        "sqlTable" : `CREATE TABLE customers (
+            id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+            code varchar(50) NOT NULL UNIQUE,
+            type varchar(50) NOT NULL,
+            name varchar(50) NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+            deleted_at TIMESTAMP NULL
+        );`
+      },
+
+      {    
+        "table" : "products",
+        "fields" : ["name","code"],
+        "range" : {
+          "value" : 10,
+          "values" : ["Pt-","Code-"]
+        },          
+        "sqlTable" : `CREATE TABLE products (
+            id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+            code varchar(50) NOT NULL UNIQUE,
+            name varchar(50) NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+            deleted_at TIMESTAMP NULL
+        );`
+      },
+
+      {
+        "table" : "purchaseings",
+        "fields" : ["code","supplier_id","user_id","total"],    
+        "datas" : [
+          ["PG-1",1,1,10000],                 
+          ["PG-2",2,2,20000]
+        ],
+        "sqlTable" : `CREATE TABLE purchaseings (
+            id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+            code varchar(50) NOT NULL UNIQUE,
+            supplier_id int DEFAULT NULL,
+            user_id int DEFAULT NULL,
+            total DECIMAL(20,2) DEFAULT 0.00,
+            grand_total DECIMAL(20,2) DEFAULT 0.00,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+            deleted_at TIMESTAMP NULL
+        );`,
+        "childs" : {
+          "table" : "purchaseing_details",
+          "fields" : ["product_id","quantity","price","amount","purchaseing_id"],    
+          "datas" : [
+            [1,1,5000,5000,1],                 
+            [2,1,5000,5000,1],                 
+            [1,2,10000,10000,2],
+            [2,2,10000,10000,2]
+          ],
+          "sqlTable" : `CREATE TABLE purchaseing_details (
+              id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+              purchaseing_id int DEFAULT NULL,
+              product_id int DEFAULT NULL,              
+              quantity DECIMAL(20,2) DEFAULT 0.00,
+              price DECIMAL(20,2) DEFAULT 0.00,
+              amount DECIMAL(20,2) DEFAULT 0.00
+          );`,
+        }
+      }
     ]
 
   const dynamicPool = new DynamicPool(4,{
@@ -39,7 +136,7 @@ try{
 
   for (const [index,data] of databases.entries()) {  
     dynamicPool.exec({
-      task: async function(data){
+      task: async (data) => {
         const mysql = this.require('mysql2/promise');
 
         const connection = await mysql.createConnection({
@@ -60,19 +157,43 @@ try{
         `);      
 
         await connection.execute(data.sqlTable);
-        
-        for(const item of data.datas){
-          let fields = data.fields.join(",");
-          let values = item.map(item => `'${item}'`).join(",");
+       
+        if(data.range){
+          for(const item of Array.from(Array(data.range.value).keys())){
+            let fields = data.fields.join(",");
+            let values = data.range.values.map(itemMap => `"${itemMap}${item}"`).join(",");
       
-          await connection.execute(`INSERT INTO ${data.table} (${fields}) VALUES (${values})`)
-        }
+            await connection.execute(`INSERT INTO ${data.table} (${fields}) VALUES (${values})`)    
+          }
+        }else{
+          for(const item of data.datas){
+            let fields = data.fields.join(",");
+            let values = item.map(itemMap => `'${itemMap}'`).join(",");
+      
+            await connection.execute(`INSERT INTO ${data.table} (${fields}) VALUES (${values})`)
+          }
+
+          if(data.childs){
+            await connection.execute(`
+              DROP TABLE IF EXISTS ${data.childs.table}
+            `);      
+  
+            await connection.execute(data.childs.sqlTable);
+            
+            for(const item of data.childs.datas){
+              let fields = data.childs.fields.join(",");
+              let values = item.map(itemMap => `'${itemMap}'`).join(",");
+      
+              await connection.execute(`INSERT INTO ${data.childs.table} (${fields}) VALUES (${values})`)
+            }  
+          }
+        }       
 
         await connection.end();
       },
       param : data      
     })
-    .then(function(){
+    .then(() => {
       console.log(`Done ${data.table}`);  
 
       doneTask += 1;
